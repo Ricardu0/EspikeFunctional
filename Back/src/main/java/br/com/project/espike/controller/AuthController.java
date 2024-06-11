6010d58e24609897ad7442e54bcda11a1395f3c7
Kell <kelvyn.queiroz@fatec.sp.gov.br>
2024-06-10 21:12:08 -0300

    Added Authentication and Login System on backend.

---
+++ Back/src/main/java/br/com/project/espike/controller/AuthController.java
@@ -0,0 +1,52 @@
+package br.com.project.espike.controller;
+
+import br.com.project.espike.model.User;
+import br.com.project.espike.repository.UserRepository;
+import br.com.project.espike.service.UserDetailsServiceImpl;
+import br.com.project.espike.util.JwtUtil;
+import br.com.project.espike.model.AuthenticationRequest;
+import br.com.project.espike.model.AuthenticationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully";
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return new AuthenticationResponse(jwt);
+    }
+}
